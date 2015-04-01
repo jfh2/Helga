01ff1feb7ec5a35d1d6955a42623e06d4e106b4c
jfh2@hi.is <jfh2@hi.is>
2015-04-01 18:59:46 +0000

    added poi

--- app/src/main/java/helgamaria/vinnuhelga/settings.java
+++ app/src/main/java/helgamaria/vinnuhelga/settings.java
@@ -12,9 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import helgamaria.vinnuhelga.sql.dbFunctions;
-
 
 public class settings extends ActionBarActivity {
     dbFunctions dbFunc = new dbFunctions(this);
