d69f43d21828040c5e1c36ea1d075799475972c1
jfh2@hi.is <jfh2@hi.is>
2015-04-08 20:08:50 +0000

    add files

---
+++ app/src/main/java/helgamaria/vinnuhelga/makeCSVFile.java
@@ -0,0 +1,82 @@
+package helgamaria.vinnuhelga;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import helgamaria.vinnuhelga.sql.JobObject;
+import helgamaria.vinnuhelga.sql.dbFunctions;
+
+/**
+ * Created by frimann on 8.4.2015.
+ */
+public class makeCSVFile {
+
+    private static final String TAG = "MEDIA";
+    public static void save(Context ctx){
+        // Find the root of the external storage.
+        // See http://developer.android.com/guide/topics/data/data-  storage.html#filesExternal
+        dbFunctions dbFunc = new dbFunctions(ctx);
+        dbFunc.open();
+        List<JobObject> listOfJobsDb = dbFunc.selectAllJobs();
+        dbFunc.close();
+
+        File root = android.os.Environment.getExternalStorageDirectory();
+
+
+        // See http://stackoverflow.com/questions/3551821/android-write-to-sd-card-folder
+
+        File dir = new File (root.getAbsolutePath() + "/download");
+        dir.mkdirs();
+        File file = new File(dir, "HelguData.txt");
+
+        try {
+            FileOutputStream f = new FileOutputStream(file);
+            PrintWriter pw = new PrintWriter(f);
+            String line = "";
+            pw.print("DATE,NAME,TYPE,STARTTIME,STOPTIME,TIME\r\n");
+            for(int i = 0; i < listOfJobsDb.size();i++){
+                DateFormat form = new SimpleDateFormat("hh:mm:ss");
+                try{
+                    Date start = form.parse(listOfJobsDb.get(i).getStartTime());
+                    Date stop = form.parse(listOfJobsDb.get(i).getStopTime());
+                    Long diff = stop.getTime() - start.getTime();
+                    System.out.println(diff/1000);
+                    String alltime = Long.toString(diff/1000);
+                    line = listOfJobsDb.get(i).getDate() +","+
+                            listOfJobsDb.get(i).getJobName() +","+
+                            listOfJobsDb.get(i).getJobType() +","+
+                            listOfJobsDb.get(i).getStartTime() +","+
+                            listOfJobsDb.get(i).getStopTime() +","+
+                            alltime+ "\r\n";
+                    pw.print(line);
+                }catch(ParseException p){
+                    p.printStackTrace();
+                }
+            }
+            pw.flush();
+            pw.close();
+            f.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.i(TAG, "******* File not found. Did you" +
+                    " add a WRITE_EXTERNAL_STORAGE permission to the   manifest?");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
